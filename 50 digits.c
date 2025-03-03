fb918660e6e9bc888b9a3432e50eef684ba1d9ce
Rogith N <115778562+Rogith710@users.noreply.github.com>
2025-03-04 00:19:32 +0530

    Update 50 digits.c
    Can now do both addition and subtraction using this code.

--- 50 digits.c
+++ 50 digits.c
@@ -1,18 +1,67 @@
 #include<stdio.h>
 #include<string.h>
-int main()
+void add(char* x,char* y,int m){
+    int z[50],q=0;
+    for(int i=m-1;i>=0;i--){
+        if(x[i]+y[i]+q-('0'+'0')>9){
+            z[i]=(((int)(x[i]+y[i]+q))-('0'+'0'))%10;
+            q=1;
+        }
+        else{
+            z[i]=((int)(x[i]+y[i]+q))-('0'+'0');
+            q=0;
+        }
+    }
+    if(q==1){
+        printf("%d",1);
+    }
+    for(int i=0;i<m;i++){
+        printf("%d",z[i]);
+    }
+}
+void sub(char* x,char* y,int m,int flag){
+    int z[50],q=0;
+    for(int i=m-1;i>=0;i--){
+        if(x[i]-(y[i]+q)<0){
+            z[i]=10+(int)(x[i]-y[i])-q;
+            q=1;
+        }
+        else{
+            z[i]=(int)(x[i]-y[i])-q;
+            q=0;
+        }
+    }
+    if(flag==1){
+        printf("-");
+    }
+    flag=1;
+    for(int i=0;i<m;i++){
+        if(flag==1 && z[i]==0){
+            
+        }
+        else{
+            flag=0;
+            printf("%d",z[i]);
+        }
+        
+    }
+}
+void main()
 {
     char x[50],y[50],z[51];
-    int q=0;
-    printf("Enter the number a:");
+    int l,flag=0;
+    printf("Enter the number a :");
     scanf("%s",x);
-    printf("Enter the number b:");
+    printf("Enter the number b :");
     scanf("%s",y);
+    printf("Enter 1 for addition and 2 for subtraction :");
+    scanf("%d",&l);
     int m,n;
-    if(strlen(x)<strlen(y)){
+    if(strlen(x)<strlen(y) || (strlen(x)==strlen(y) && x<y)){
         strcpy(z,x);
         strcpy(x,y);
         strcpy(y,z);
+        flag = 1;
     }
     m=strlen(x);
     n=strlen(y);
@@ -20,46 +69,30 @@ int main()
     for(int i=0;i<m;i++){
         if(i<n && ((int)x[i]<48 || (int)x[i]>57 ||  (int)y[i]<48 || (int)y[i]>57)){
             printf("Invalid Input");
-            return 0;
+            return;
         }
         else if((int)x[i]<48 || (int)x[i]>57){
             printf("Invalid Input");
-            return 0;
+            return;
         }
     }
-    for(int i=m-1,j=n-1;i>=0;i--){
-        
-        if(j>=0){
-            if((int)x[i]-48+(int)y[j]-48+q>9){
-            z[m-i-1]=(char)((((int)x[i]-48+(int)y[j]-48+q)%10)+48);
-            q=1;
-         }
-            else{
-            z[m-i-1]=(char)(((int)x[i]-48+(int)y[j]-48+q)+48);
-            q=0;
-         }
-         j--;
+    for(int i=m-1,j=n-1;i>=0;i--,j--){
+        if(i>m-n-1){
+            y[i]=y[j];
         }
         else{
-            if(z[m-i-1]=(char)((int)x[i]+q-48)>9){
-                z[m-i-1]=(char)((((int)x[i]+q-48)%10)+48);
-                q=1;
-            }
-            else{
-                z[m-i-1]=(char)((int)x[i]+q);
-                q=0;
-            }
+            y[i]='0';
         }
-        
     }
-    printf("The sum of the numbers a and b is ");
-    if(q==1){
-        printf("%d",q);
+    if(l==1){
+        add(x,y,m);
     }
-    for(int i=m-1;i>=0;i--){
-        printf("%c",z[i]);
+    else if(l==2){
+        sub(x,y,m,flag);
     }
-    return 0;
-
+    else{
+        printf("Invalid Input");
+    }
+    return;
 
 }
